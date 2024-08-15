97d2aa8ea993624b07885103bf4a853ca993a944
Georginio-prod <etonameklou6@gmail.com>
2024-08-15 23:48:02 +0000

    style: creation and merge mobile card

--- src/components/toggle.vue
+++ src/components/toggle.vue
@@ -1,16 +1,16 @@
 <template>
     <div
-        class="min-h-screen max-w-sm h-full w-full flex flex-col items-center justify-center bg-background bg-[url('/public/bg-top.svg')] px-6">
+        class="min-h-screen max-w-sm h-full w-full flex flex-col items-center justify-center bg-background bg-[url('/public/bg-top.svg')] bg-right bg-no-repeat  px-6">
 
-        <div class="text-center px-16 pt-16 pb-20">
-            <p>
+        <div class="text-center px-16 pt-16 pb-20 text-grisclair font-bold">
+            <p class=" text-4xl">
                 Our Pricing
             </p>
 
             <div class=" flex items-center pt-10">
-                <div class="flex items-center gap-6">
-                    <span :class="{ 'opacity-50 ': isYearly }">
-                        Annually
+                <div class="flex items-center gap-6 ">
+                    <span :class="{isYearly}">
+                        <h1 class="opacity-50">Annually</h1>
                     </span>
                     <label class="relative flex items-center cursor-pointer ">
                         <input type="checkbox" class="sr-only peer" v-model="isYearly" />
@@ -23,15 +23,128 @@
                         </span>
 
                     </label>
-                    <span :class="{ 'opacity-50': !isYearly }">
-                        Monthly
+                    <span :class="{ '': !isYearly }">
+                        <h1 class="opacity-50">Monthly</h1>
                     </span>
 
                 </div>
             </div>
         </div>
 
-      
+        <div class="flex flex-col items-center justify-center w-full gap-8 mb-20">
+            <div class="text-center w-full flex flex-col items-center justify-center px-7 font-bold bg-white py-8 rounded-lg shadow-2xl">
+                <p class="w-full pb-6 text-lg text-grisclair">
+                    Basic
+                </p>
+
+                <div class="flex flex-col w-full ">
+                    <h1 class="w-full pb-8 text-4xl text-number">
+                        <p v-if="!isYearly" class="tracking-normal items-center flex justify-center "> &dollar;<span class="font-bold text-7xl pl-2">19.99</span></p>
+                        <p v-else class="tracking-normal items-center flex justify-center"> &dollar;<span class="font-bold text-7xl pl-2">199.99</span></p>
+                    </h1>
+
+                    <table class="">
+                        <thead class="">
+                            <tr class="border-b">
+
+                            </tr>
+                        </thead>
+                        <tbody class="text-grisclair text-base">
+                            <tr class="border-b">
+                                <th class="p-4">500 GB Storage</th>
+                            </tr>
+                            <tr class="border-b">
+                                <th class="p-4">2 Users Allowed</th>
+                            </tr>
+                            <tr class="border-b">
+                                <th class="p-4">Send up to 3 GB</th>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <button @click="togglePrice"
+                        class="text-sm font-bold text-white bg-gradient-to-b from-linear to-linear1 py-3 mt-10 w-full">
+                        LEARN MORE
+                    </button>
+                </div>
+            </div>
+
+            <div class="text-center w-full flex flex-col items-center justify-center px-7 font-bold bg-gradient-to-br from-linear to-linear1 py-8 rounded-lg text-white shadow-2xl">
+                <p class="w-full pb-6 text-lg ">
+                    Professional
+                </p>
+
+                <div class="flex flex-col w-full ">
+                    <h1 class="w-full pb-8 text-4xl">
+                        <p v-if="!isYearly" class="tracking-normal items-center flex justify-center "> &dollar;<span class="font-bold text-7xl pl-2">24.99</span></p>
+                        <p v-else class="tracking-normal items-center flex justify-center"> &dollar;<span class="font-bold text-7xl pl-2">249.99</span></p>
+                    </h1>
+
+                    <table class="">
+                        <thead class="">
+                            <tr class="border-b">
+
+                            </tr>
+                        </thead>
+                        <tbody class=" text-base">
+                            <tr class="border-b">
+                                <th class="p-4">  1 TB Storage</th>
+                            </tr>
+                            <tr class="border-b">
+                                <th class="p-4">5 Users Allowed</th>
+                            </tr>
+                            <tr class="border-b">
+                                <th class="p-4">Send up to 10 GB</th>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <button @click="togglePrice"
+                        class="text-sm font-bold text-btn bg-white py-3 mt-10 w-full">
+                        LEARN MORE
+                    </button>
+                </div>
+            </div>
+
+            <div class="text-center w-full flex flex-col items-center justify-center px-7 font-bold bg-white py-8 rounded-lg shadow-2xl">
+                <p class="w-full pb-6 text-lg text-grisclair">
+                    Master
+                </p>
+
+                <div class="flex flex-col w-full ">
+                    <h1 class="w-full pb-8 text-4xl text-number">
+                        <p v-if="!isYearly" class="tracking-normal items-center flex justify-center "> &dollar;<span class="font-bold text-7xl pl-2">39.99</span></p>
+                        <p v-else class="tracking-normal items-center flex justify-center"> &dollar;<span class="font-bold text-7xl pl-2">399.99</span></p>
+                    </h1>
+
+                    <table class="">
+                        <thead class="">
+                            <tr class="border-b">
+
+                            </tr>
+                        </thead>
+                        <tbody class="text-grisclair text-base">
+                            <tr class="border-b">
+                                <th class="p-4">2 TB Storage</th>
+                            </tr>
+                            <tr class="border-b">
+                                <th class="p-4">10 Users Allowed</th>
+                            </tr>
+                            <tr class="border-b">
+                                <th class="p-4">Send up to 20 GB</th>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <button @click="togglePrice2"
+                        class="text-sm font-bold text-white bg-gradient-to-b from-linear to-linear1 py-3 mt-10 w-full ">
+                        LEARN MORE
+                    </button>
+                </div>
+            </div>
+          
+        </div>
+
 
 
 
@@ -49,6 +162,9 @@ export default {
     },
     methods: {
         togglePrice() {
+            this.isYearly = !this.isYearly
+        },
+        togglePrice2(){
             this.isYearly = !this.isYearly
         }
     }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ export default {
         background : "#F7F7FE",
         linear: "#A2A7F0",
         linear1: "#696EDD",
+        grisclair: "#6E728E",
+        number: "#4A4D60",
+        btn:"#6D72DE"
       }
     },
   },
